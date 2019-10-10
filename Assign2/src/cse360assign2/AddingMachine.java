52e0543f8dcb777e0ccf421ce3742961b530c105
jbinugeo <jbinugeo@DESKTOP-2DTI5QJ>
2019-10-09 23:20:33 -0700

    Initial version

---
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,57 @@
+/*
+Name: Joel Binu George
+Class ID: 8515 F - 9:40 - 10:30 AM
+Description: This program is a class file that adds subtracts, gets the total 
+and also print out the expression in the form of a string
+*/
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This method starts from 0 and appends 0 into the expression string
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method gets the total of the operations and returns total
+	 * @return returning the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method adds the value that is inputted into the expressions
+	 * @param value value that is used to be manipulated by the method
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method subtracts the value that is inputted into the expressions
+	 * @param value value that is used to be manipulated by the method
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method returns all the expressions that were inputted as a string
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * This method clears the running total that was gotten before and turns in into 0
+	 */
+	public void clear() {
+	
+	}
+}
